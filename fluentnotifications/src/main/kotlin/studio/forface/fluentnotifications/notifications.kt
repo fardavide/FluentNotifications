ea69d4810a8f5dc299575675f545b0ab4b59dfa7
4face-studi0 <37957092+4face-studi0@users.noreply.github.com>
2019-04-21 21:59:40 +0200

    Added `showNotification` and `cancelNotification` by ids

--- fluentnotifications/src/main/kotlin/studio/forface/fluentnotifications/notifications.kt
+++ fluentnotifications/src/main/kotlin/studio/forface/fluentnotifications/notifications.kt
@@ -3,20 +3,43 @@
 package studio.forface.fluentnotifications
 
 import android.content.Context
+import androidx.annotation.IntegerRes
+import androidx.annotation.StringRes
 import studio.forface.fluentnotifications.enum.NotificationImportance
 import studio.forface.fluentnotifications.utils.Android
 import studio.forface.fluentnotifications.utils.notificationManager
 
 /**
  * Show a Notification created with Dsl from a [Context]
- * @param id a required id for create the Notification
+ * @param idRes a REQUIRED [IntegerRes] id for create the Notification
+ * @param tagRes an OPTIONAL [StringRes] tag for create the Notification. If `null` if will be ignored
+ * Default is `null`
+ *
+ * @see NotificationDsl as [DslMarker]
+ */
+@NotificationDsl
+fun Context.showNotification(
+    @IntegerRes idRes: Int,
+    @StringRes tagRes: Int? = null,
+    block: NotificationCoreBuilder.() -> Unit
+) {
+    showNotification( resources.getInteger( idRes ), tagRes?.let( ::getString ), block  )
+}
+
+/**
+ * Show a Notification created with Dsl from a [Context]
+ * @param id a REQUIRED id for create the Notification
  * @param tag an OPTIONAL tag for create the Notification. If `null` if will be ignored
  * Default is `null`
  *
  * @see NotificationDsl as [DslMarker]
  */
 @NotificationDsl
-fun Context.showNotification( id: Int, tag: String? = null, block: NotificationCoreBuilder.() -> Unit ) {
+fun Context.showNotification(
+    id: Int,
+    tag: String? = null,
+    block: NotificationCoreBuilder.() -> Unit
+) {
     val builder = NotificationCoreBuilder( this ).apply( block )
 
     with( notificationManager ) {
@@ -27,7 +50,17 @@ fun Context.showNotification( id: Int, tag: String? = null, block: NotificationC
 
 /**
  * Cancel a Notification form a [Context]
- * @param id required id for find the Notification to cancel
+ * @param idRes REQUIRED [IntegerRes] id for find the Notification to cancel
+ * @param tagRes OPTIONAL [StringRes] tag for find the Notification. If `null` if will be ignored
+ * Default is `null`
+ */
+fun Context.cancelNotification( @IntegerRes idRes: Int, @StringRes tagRes: Int? = null ) {
+    cancelNotification( resources.getInteger( idRes ), tagRes?.let( ::getString ) )
+}
+
+/**
+ * Cancel a Notification form a [Context]
+ * @param id REQUIRED id for find the Notification to cancel
  * @param tag OPTIONAL tag for find the Notification. If `null` if will be ignored
  * Default is `null`
  */
